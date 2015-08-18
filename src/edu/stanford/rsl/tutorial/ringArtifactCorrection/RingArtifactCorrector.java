349e2613cd61ef5810f0e20fe080b069c21d1665
Florian Gabsteiger <florian.g@me.com>
2015-08-18 22:20:14 +0200

    Added class and method comments.

--- src/edu/stanford/rsl/tutorial/ringArtifactCorrection/RingArtifactCorrector.java
+++ src/edu/stanford/rsl/tutorial/ringArtifactCorrection/RingArtifactCorrector.java
@@ -9,6 +9,26 @@ import edu.stanford.rsl.conrad.filtering.MeanFilteringTool;
 import edu.stanford.rsl.conrad.filtering.MedianFilteringTool;
 import edu.stanford.rsl.conrad.utils.ImageUtil;
 
+/*
+ * The ring artifact correction algorithm was implemented according to Prell et al. 
+ * using the correction in polar coordinates. This implementation includes two classes: 
+ * The PolarConverter to transform the input image (Grid2D) into polar coordinates, 
+ * and the RingArtifactCorrector class that performs the actual correction. 
+ *
+ * The code also uses the automatic object segmentation from Sijbers, J., & Postnov 
+ * which removes the background/I0 regions before running the correction. 
+ * The implementation might therefore fail for ROI scans.
+ * 
+ * An example on how to use the correction is shown in RingArtifactCorrectionExample. 
+ * The options include:
+ * .setShowSteps(true); to display all the steps performed during the correction.
+ * .setRadialFilterWidth; to input the radial filter width
+ * .setAzimuthalFilterWidth; to input the azimuthal filter width
+ * 
+ * The algorithm is implemented for a single slice. To correct a complete 3D volume each
+ * slice needs to be iterated. 
+ */
+
 public class RingArtifactCorrector {
 	private int radialFilterWidth, azimuthFilterWidth; //filter widths
 	private boolean showSteps = false; //show images for each step
@@ -23,21 +43,26 @@ public class RingArtifactCorrector {
 		this.azimuthFilterWidth = 40;
 	}
 	
+	/*
+	 * Main correction method, including the automatic object segmentation.
+	 * @input: Single reconstruction slice with ring artifacts
+	 * @return: Ring artifact corrected slice
+	 */
 	public Grid2D getRingArtifactCorrectedImage(Grid2D input) {
-		ImagePlus mask = getMask(input);
-		Grid2D thresholded = thresholdImage(input,mask);
+		ImagePlus mask = getMask(input); //automatic object segmentation
+		Grid2D thresholded = thresholdImage(input,mask); //remove background
 		PolarConverter conv = new PolarConverter();
-		Grid2D polarConverted = conv.convertToPolar(thresholded);
-		Grid2D radialFilt = radialMedianFilter((Grid2D) polarConverted.clone(),radialFilterWidth);
-		Grid2D diff = subtractImage((Grid2D) polarConverted.clone(),radialFilt);
-		Grid2D aziFilt = azimuthalMedianFilter((Grid2D) diff.clone(), azimuthFilterWidth);
-		Grid2D artifactsCart = conv.convertToCartesian((Grid2D) aziFilt.clone());
-		Grid2D result = subtractImage(input,artifactsCart);
+		Grid2D polarConverted = conv.convertToPolar(thresholded); //convert object to polar coordinates
+		Grid2D radialFilt = radialMedianFilter((Grid2D) polarConverted.clone(),radialFilterWidth); //first filtering
+		Grid2D diff = subtractImage((Grid2D) polarConverted.clone(),radialFilt); //remove radial filtered artifacts 
+		Grid2D aziFilt = azimuthalMedianFilter((Grid2D) diff.clone(), azimuthFilterWidth); //azimuthal filtering of extracted ring artifacts
+		Grid2D artifactsCart = conv.convertToCartesian((Grid2D) aziFilt.clone()); //convert ring artifacts back to cartesian coordinates
+		Grid2D result = subtractImage(input,artifactsCart); //subtract segmented ring artifacts from original image
 		ImagePlus res = ImageUtil.wrapGrid(result, "RingArtifact Corrected");
 		res.getProcessor().resetMinAndMax();
 		result = ImageUtil.wrapImageProcessor(res.getProcessor());
-		result = mathMin(result);
-		if (showSteps) {
+		result = mathMin(result); //fix values below 0
+		if (showSteps) { //display the different steps/images during the correction
 			mask.show("Threshold mask");
 			thresholded.show("Thresholded");
 			polarConverted.show("Converted to polar coordinates");
@@ -53,6 +78,10 @@ public class RingArtifactCorrector {
 		this.radialFilterWidth = width;
 	}
 	
+	/*
+	 * Setter method for mean filter width in azimuthal direction.
+	 * @width: Filter width. Should be odd - but method will approximate if even width is set.
+	 */
 	public void setAzimuthalFilterWidth (int width) {
 		this.azimuthFilterWidth = width;
 	}
@@ -61,6 +90,9 @@ public class RingArtifactCorrector {
 		this.showSteps = show;
 	}
 	
+	/*
+	 * Sets all values (in the corrected slice) that are below 0 to 0
+	 */
 	private static Grid2D mathMin(Grid2D slice) {
 		Grid2D res = (Grid2D) slice.clone();
 		for (int x=0; x<res.getWidth(); x++) {
@@ -73,7 +105,12 @@ public class RingArtifactCorrector {
 		return res;
 	}
 	
-	private static ImagePlus getMask(Grid2D slice) { //automatic object segmentation
+	/*
+	 * Calculates object mask (segmentation) according to Sijbers, J., & Postnov.
+	 * If ROI scan is used or this segmentation failes (check "showSteps" option) this method
+	 * should be checked first. 
+	 */
+	private static ImagePlus getMask(Grid2D slice) { 
 		IJ.showStatus("Thresholding...");
 		ImagePlus imp = ImageUtil.wrapGrid(slice, "mask");
 		ImageProcessor proc = imp.getProcessor();
@@ -88,6 +125,9 @@ public class RingArtifactCorrector {
 		return imp;
 	}
 	
+	/*
+	 * Sets all background pixels (previously determined in "getMask" method) of the input image to 0
+	 */
 	private static Grid2D thresholdImage(Grid2D slice, ImagePlus mask) {
 		Grid2D res = (Grid2D) slice.clone();
 		for (int x=0; x<res.getWidth(); x++) {
@@ -100,6 +140,11 @@ public class RingArtifactCorrector {
 		return res;
 	}
 	
+	/*
+	 * First filtering step after conversion to polar coordinates, along the radial direction.
+	 * Image is divided in 3 separate regions (inner, middle, outer "ring") and filtered with
+	 * different filter widths.
+	 */
 	private static Grid2D radialMedianFilter(Grid2D polar, int filterW) {
 		IJ.showStatus("Radial median filtering...");
 		MedianFilteringTool medfilt = new MedianFilteringTool();
@@ -116,6 +161,10 @@ public class RingArtifactCorrector {
 		return merged;
 	}
 	
+	/*
+	 * Second filtering step. Mean filter along the azimuthal direction.
+	 * @filterW: filter width. Should be odd, but this method corrects even values to the closest odd width.
+	 */
 	private static Grid2D azimuthalMedianFilter(Grid2D polar, int filterW) {
 		IJ.showStatus("Azimuthal low-pass filtering...");
 		Grid2D[] subGrids = radialFilterAreas(polar);
@@ -145,6 +194,9 @@ public class RingArtifactCorrector {
 		return merged;
 	}
 	
+	/*
+	 * Merges the three different regions after filtering. 
+	 */
 	private static Grid2D mergeRadialAreas(Grid2D[] subGrids) {
 		int subWidth0 = subGrids[0].getWidth();
 		int subWidth1 = subGrids[1].getWidth();
@@ -168,7 +220,10 @@ public class RingArtifactCorrector {
 	}
 
 	
-	
+	/*
+	 * Divides polar converted image into three subregions: inner, middle, outer "ring".
+	 * @return: array of three Grid2Ds. First: inner, Second: middle, Third: outer region. 
+	 */
 	private static Grid2D[] radialFilterAreas(Grid2D polar) {
 		int[] areas = radialFilterLimits(polar);
 		Grid2D inner = new Grid2D(areas[0],polar.getHeight());
@@ -188,7 +243,9 @@ public class RingArtifactCorrector {
 		return subGrids;
 	}
 	
-	
+	/*
+	 * Calculates borders between filter regions.
+	 */
 	private static int[] radialFilterLimits(Grid2D polar) {
 		int[] areas = new int[3];
 		int width = polar.getWidth();
@@ -198,6 +255,9 @@ public class RingArtifactCorrector {
 		return areas;
 	}
 	
+	/*
+	 * Subtracts the calculated ring artifact image from the original input image.
+	 */
 	private static Grid2D subtractImage(Grid2D polarInput, Grid2D artifacts) {
 		IJ.showStatus("Getting difference image...");
 		Grid2D res = (Grid2D) polarInput.clone();
@@ -211,7 +271,10 @@ public class RingArtifactCorrector {
 		}
 		return res;
 	}
-
+	
+	/*
+	 * Helper method to load image file as Grid2D.
+	 */
 	Grid3D loadReconstruction(String path) {
 		Grid3D impAsGrid = null;
 		try {
