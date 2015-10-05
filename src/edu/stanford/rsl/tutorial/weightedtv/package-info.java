0ba34e22c38a1e278f69e36c32dac714e800fe8f
YixingHuang <yixing.yh.huang@fau.de>
2015-10-05 21:34:23 +0200

    Create package-info.java

---
+++ src/edu/stanford/rsl/tutorial/weightedtv/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package contains iterative weighted total variation experiments.
+ *
+ * @author Andreas Maier
+ */
+package edu.stanford.rsl.tutorial.weightedtv;
+/*
+ * Copyright (C) 2010-2014 Andreas Maier
+ * CONRAD is developed as an Open Source project under the GNU General Public License (GPL).
+*/
