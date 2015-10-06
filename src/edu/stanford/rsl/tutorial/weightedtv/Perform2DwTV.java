c6bbe61b576acf6d0818c45b60936e2cbccba1e4
YixingHuang <yixing.yh.huang@fau.de>
2015-10-06 13:13:35 +0200

    Update Perform2DwTV.java

--- src/edu/stanford/rsl/tutorial/weightedtv/Perform2DwTV.java
+++ src/edu/stanford/rsl/tutorial/weightedtv/Perform2DwTV.java
@@ -21,24 +21,16 @@ import edu.stanford.rsl.tutorial.phantoms.SheppLogan;
 import edu.stanford.rsl.tutorial.fan.FanBeamProjector2D;
 import edu.stanford.rsl.tutorial.fan.FanBeamBackprojector2D;
 
-//import TVreconstruction.FanBeamProjector2D;
-//import edu.stanford.rsl.tutorial.fan.FanBeamProjector2D;
-
 /**
+ *This is an example to apply weighted total variation (wTV) on 2D fan-beam limited angle tomography.
+ *wTV is according to [1].
  *[1]Candes. Enhancing Sparsity by Reweighted l1 Minimization
- * 
- * 
  * @author Yixing Huang
  *
  */
-public class Perform2DwTV {
+public class Perform2DWeightedTV {
 	static boolean isOpencl=true;
-	
-	
-	 private static String path="D:\\wTV\\";
-	 private static File outPutDir=new File(path+"log.txt");
-	 private static BufferedWriter bw;
-	
+
 	 static int maxIter = 500;
 	 static int maxTVIter = 10;
 	 static double epsilon = 1.0e-10;
@@ -47,18 +39,18 @@ public class Perform2DwTV {
 	 static int iter = 0;
 	 static double step=0;
 	
-	
+	//SART
 	static Grid2D phan;
 	static Grid2D sinogram;
-	static Grid1D sinodiff1D;
+	static Grid1D sinoDiff1D;
 	static Grid2D recon;
-	static Grid2D recon_pre;
-	static Grid2D wSinogram;
-	static Grid2D wGrids;
+	static Grid2D reconPre;
+	static Grid2D normSinogram;
+	static Grid2D normGrids;
 	static Grid2D localImageUpdate;
-	//wTV
-	static TVGradient tvGrad;
-	static Grid2D tv_Gradient;
+	//weighted TV
+	static TVGradient TVGrad;
+	static Grid2D TVGradient;
 	//Geometry
 	static int imgSizeX;
 	static int imgSizeY;
@@ -70,7 +62,7 @@ public class Perform2DwTV {
 	static double deltaBeta;
 	static int spacingX=1,spacingY=1;
 	static FanBeamProjector2D projector;
-	static FanBeamBackprojector2D backproj;
+	static FanBeamBackprojector2D backProj;
 	
 	public static void main(String[] args) throws IOException {
 		new ImageJ();
@@ -81,12 +73,6 @@ public class Perform2DwTV {
 		phan.setSpacing(spacingX,spacingY);
 		phan.show("The Phantom");
 	   	
-		if(!outPutDir.exists()){
-		    outPutDir.getParentFile().mkdirs();
-			outPutDir.createNewFile();
-		}
-		
-		bw=new BufferedWriter(new FileWriter(outPutDir));
 	   
 	   initialGeometry();
 	   initialSART();
@@ -95,49 +81,51 @@ public class Perform2DwTV {
        
 	
        //initial wTV
-		tv_Gradient= new Grid2D(imgSizeX, imgSizeY);
-		tv_Gradient.setSpacing(1, 1);
-		tvGrad = new TVGradient(recon);
-		tvGrad.initialWmatrix();
+		TVGradient= new Grid2D(imgSizeX, imgSizeY);
+		TVGradient.setSpacing(1, 1);
+		TVGrad = new TVGradient(recon);
+		TVGrad.initialWeightMatrix();
    
 
 		
 		while (error > epsilon && iter <=maxIter) {
 		 sartIterate();		 
-		 wTVIterate();
-		 System.out.println(iter+"wTV="+tvGrad.getwTVvalue(recon));
-		 tvGrad.wMatrixUpdate(recon);
+		 weightedTVIterate();
+
+		 TVGrad.weightMatrixUpdate(recon);
 		 outPutError();
 		 iter++;
 		}
-		bw.close();
+
 		recon.show("Reconstructed Image");
 	}
 	
 	
 	
-
+/**
+ * SART iteration part
+ */
 private static void sartIterate(){
-	recon_pre=new Grid2D(recon);
+	reconPre=new Grid2D(recon);
 	for (int theta = 0; theta < sinogram.getSize()[1]; theta++) {
 		if(isOpencl)
-			sinodiff1D= projector.projectRayDriven1DCL(recon, theta);//get the projection of the current updated image at angle theta
+			sinoDiff1D= projector.projectRayDriven1DCL(recon, theta);//get the projection of the current updated image at angle theta
 		else
-			sinodiff1D = projector.projectRayDriven1D(recon, theta);
+			sinoDiff1D = projector.projectRayDriven1D(recon, theta);
 		  
-		sinodiff1D=(Grid1D)NumericPointwiseOperators.subtractedBy(sinodiff1D, sinogram.getSubGrid(theta));//compare with the measured sinogram, get the difference
+		sinoDiff1D=(Grid1D)NumericPointwiseOperators.subtractedBy(sinoDiff1D, sinogram.getSubGrid(theta));//compare with the measured sinogram, get the difference
 
-		NumericPointwiseOperators.divideBy(sinodiff1D, wSinogram.getSubGrid(theta));
+		NumericPointwiseOperators.divideBy(sinoDiff1D, normSinogram.getSubGrid(theta));
 
 		if(isOpencl)
-			localImageUpdate = backproj.backprojectPixelDriven1DCL(sinodiff1D, theta);
+			localImageUpdate = backProj.backprojectPixelDriven1DCL(sinoDiff1D, theta);
 		else
-			localImageUpdate = backproj.backprojectPixelDriven1D(sinodiff1D, theta);
+			localImageUpdate = backProj.backprojectPixelDriven1D(sinoDiff1D, theta);
 	
 		double stepSize = -1.0;
 
 		NumericPointwiseOperators.multiplyBy(localImageUpdate,(float) stepSize);
-		NumericPointwiseOperators.divideBy(localImageUpdate, wGrids);
+		NumericPointwiseOperators.divideBy(localImageUpdate, normGrids);
 	
        
 		NumericPointwiseOperators.addBy(recon, localImageUpdate);
@@ -146,17 +134,20 @@ private static void sartIterate(){
 	 recon.getGridOperator().removeNegative(recon);
 }
 
-private static void wTVIterate()
+/**
+ * weighted TV gradient descent part
+ */
+private static void weightedTVIterate()
 {
-	double TV=tvGrad.getwTVvalue(recon);
+	double TV=TVGrad.getWeightedTVvalue(recon);
 	int i=0;
 	double deltaTV=-1.0;
 	while(i<maxTVIter&& deltaTV<0){
 		double preTV=TV;
-		tv_Gradient=tvGrad.computewTVGradient(recon);
-		NumericPointwiseOperators.divideBy(tv_Gradient, (float) tvGrad.max_Value);
-		wBTLsearch(tv_Gradient);//****************
-		TV=tvGrad.getwTVvalue(recon);
+		TVGradient=TVGrad.computewTVGradient(recon);
+		NumericPointwiseOperators.divideBy(TVGradient, (float) TVGrad.maxValue);
+		backTrackingLineSearch(TVGradient);//****************
+		TV=TVGrad.getWeightedTVvalue(recon);
 		
 		deltaTV=TV-preTV;
 		System.out.println(iter+" i="+i+" wTV="+TV+" step="+step);
@@ -164,15 +155,19 @@ private static void wTVIterate()
 	} 
 }
 
-private static void wBTLsearch(Grid2D grad){//weighted TV
+/**
+ * Using back tracking line search algorithm to find the step size for weighted TV gradient descent
+ * @param grad
+ */
+private static void backTrackingLineSearch(Grid2D grad){//weighted TV
 	double t=1.0,tmin=1.0e-7;
 	double alpha=0.3, beta=0.6;
 	double delta=1.0f,temp1,temp2;
-	double TV=tvGrad.getwTVvalue(recon);
-	double Gradnorm=alpha*grid2Dnorm(grad);
+	double TV=TVGrad.getWeightedTVvalue(recon);
+	double Gradnorm=alpha*grid2DNorm(grad);
 	Grid2D temp=new Grid2D(grad);
 	NumericPointwiseOperators.multiplyBy(temp,(float) t);
-	temp1=tvGrad.getwTVvalue((Grid2D)NumericPointwiseOperators.subtractedBy(recon,temp)); 
+	temp1=TVGrad.getWeightedTVvalue((Grid2D)NumericPointwiseOperators.subtractedBy(recon,temp)); 
 	temp2=t*Gradnorm;
 	delta=temp1-TV+temp2;
 	while(delta>0.0f&& t>tmin)
@@ -180,7 +175,7 @@ private static void wBTLsearch(Grid2D grad){//weighted TV
 		t=t*beta;
 		temp=(Grid2D)grad.clone();
 		NumericPointwiseOperators.multiplyBy(temp,(float) t);
-		temp1=tvGrad.getwTVvalue((Grid2D)NumericPointwiseOperators.subtractedBy(recon,temp)); //
+		temp1=TVGrad.getWeightedTVvalue((Grid2D)NumericPointwiseOperators.subtractedBy(recon,temp)); //
 		temp2=t*Gradnorm;
 		delta=temp1-TV+temp2;
 	}
@@ -188,6 +183,9 @@ private static void wBTLsearch(Grid2D grad){//weighted TV
 	NumericPointwiseOperators.subtractBy(recon, temp);;
 }
 
+/**
+ * The limited angle tomography geometry
+ */
 private static void initialGeometry()
 {
 	gammaM = 10. * Math.PI / 180;
@@ -203,91 +201,109 @@ private static void initialGeometry()
 	deltaBeta = 1.0 * Math.PI / 180.0;
 	projector = new FanBeamProjector2D(
 			focalLength, maxBeta, deltaBeta, maxT, deltaT);
-    backproj=new FanBeamBackprojector2D(focalLength,deltaT, deltaBeta, imgSizeX, imgSizeY);  
+    backProj=new FanBeamBackprojector2D(focalLength,deltaT, deltaBeta, imgSizeX, imgSizeY);  
 }
 
+/**
+ * Initial SART grids
+ */
 private static void initialSART(){
     if(isOpencl)
 	      sinogram = (Grid2D) projector.projectRayDrivenCL(phan);
     else
 	   sinogram = (Grid2D) projector.projectRayDriven(phan);
     sinogram.show("sinogram");
-    backproj.initSinogramParams(sinogram);
+    backProj.initSinogramParams(sinogram);
 	
 		recon = new Grid2D(imgSizeX, imgSizeY);
 		recon.setSpacing(spacingX, spacingY);
 		NumericPointwiseOperators.fill(recon, 0);// initialization
 		
 		
-		sinodiff1D = new Grid1D(sinogram.getSize()[0]);
-		sinodiff1D.setSpacing(deltaT);
+		sinoDiff1D = new Grid1D(sinogram.getSize()[0]);
+		sinoDiff1D.setSpacing(deltaT);
 		localImageUpdate = new Grid2D(imgSizeX, imgSizeY);
 		localImageUpdate.setSpacing(spacingX, spacingY);
 		localImageUpdate.setOrigin(imgSizeX/2,imgSizeY/2);
  
 }
 
+/**
+ * compute the normalization projections
+ */
+private static void createNormProj(){
+	//projection normalization weights
+	Grid2D C_phan=new Grid2D(imgSizeX,imgSizeY);//Constant grid with all values as 1;
+	C_phan.setSpacing(spacingX,spacingY);
+	NumericPointwiseOperators.fill(C_phan,1.0f);
 
-	private static void createNormProj(){
-		//projection normalization weights
-		Grid2D C_phan=new Grid2D(imgSizeX,imgSizeY);//Constant grid with all values as 1;
-		C_phan.setSpacing(spacingX,spacingY);
-		NumericPointwiseOperators.fill(C_phan,1.0f);
-
-		if(isOpencl)
-			wSinogram=(Grid2D)projector.projectRayDrivenCL(C_phan);
-		else
-			wSinogram=(Grid2D)projector.projectRayDriven(C_phan);
-		NumericPointwiseOperators.addBy(wSinogram,(float) epsilon);
+	if(isOpencl)
+		normSinogram=(Grid2D)projector.projectRayDrivenCL(C_phan);
+	else
+		normSinogram=(Grid2D)projector.projectRayDriven(C_phan);
+	NumericPointwiseOperators.addBy(normSinogram,(float) epsilon);
 	}
 
-	private static void createNormGrids(){
-		//backprojection normalization weights
-		Grid1D C_sino1D=new Grid1D(sinogram.getSize()[0]);
-		C_sino1D.setSpacing(deltaT);
-		NumericPointwiseOperators.fill(C_sino1D,1.0f);
+/**
+ * compute normalization grids for backprojection
+ */
+private static void createNormGrids(){
+	//backprojection normalization weights
+	Grid1D C_sino1D=new Grid1D(sinogram.getSize()[0]);
+	C_sino1D.setSpacing(deltaT);
+	NumericPointwiseOperators.fill(C_sino1D,1.0f);
 
-		wGrids=new Grid2D(imgSizeX,imgSizeY);
-		if(isOpencl)
-			wGrids=backproj.backprojectPixelDriven1DCL(C_sino1D, 0);
-		else
-			wGrids=backproj.backprojectPixelDriven1D(C_sino1D, 0);
+	normGrids=new Grid2D(imgSizeX,imgSizeY);
+	if(isOpencl)
+		normGrids=backProj.backprojectPixelDriven1DCL(C_sino1D, 0);
+	else
+		normGrids=backProj.backprojectPixelDriven1D(C_sino1D, 0);
 	
-		NumericPointwiseOperators.addBy(wGrids,(float) epsilon);
-	}
-	
-	private static void outPutError() throws IOException{
-		if (iter % 5== 0){
-			recon.show(iter+"_th iteration");
-			ImagePlus imp2=ImageUtil.wrapGrid(recon, null);
-			IJ.saveAs(imp2, "Tiff", (path+iter+"thResult.tif"));	
-		}
-		error =mse(recon, recon_pre);	
-		error2=mse(recon, phan);
-		System.out.println(iter+":  error=" + error+" error2= "+error2);
-		bw.write(iter+":  error=" + error+" error2= "+error2+"\r\n");
-		bw.flush();	
+	NumericPointwiseOperators.addBy(normGrids,(float) epsilon);
 	}
 
-	private static double mse(Grid2D recon, Grid2D recon_data) {
-		double err = 0;
-		Grid2D temp = new Grid2D(recon);
-		NumericPointwiseOperators.subtractBy(temp, recon_data);
-		err = grid2Dnorm(temp);
-		err=err / (temp.getSize()[0] * temp.getSize()[1]);
+/**
+ * output the difference between too iterations and output the difference from the ground truth	
+ * @throws IOException
+ */
+private static void outPutError() throws IOException{
+	if (iter % 5== 0){
+		recon.show(iter+"_th iteration");	
+	}
+	error =meanSquareError(recon, reconPre);	
+	error2=meanSquareError(recon, phan);
+	System.out.println(iter+":  error=" + error+" error2= "+error2);
+}
+
+/**
+ * mean square error
+ * @param imgGrid1
+ * @param imgGrid2
+ * @return
+ */
+private static double meanSquareError(Grid2D imgGrid1, Grid2D imgGrid2) {
+	double err = 0;
+	Grid2D temp = new Grid2D(imgGrid1);
+	NumericPointwiseOperators.subtractBy(temp, imgGrid2);
+	err = grid2DNorm(temp);
+	err=err / (temp.getSize()[0] * temp.getSize()[1]);
 		
-		return err;
-	}
+	return err;
+}
 	
+/**
+ * L2 norm
+ * @param imgGrid
+ * @return
+ */
+private static double grid2DNorm(Grid2D imgGrid) {
 
-	private static double grid2Dnorm(Grid2D recon) {
-		
-		double d = 0;
-		for (int row = 0; row < recon.getSize()[0]; row++)
-			for (int col = 0; col < recon.getSize()[1]; col++)
-				d = d + recon.getAtIndex(row, col) * recon.getAtIndex(row, col);
-		return d;
-	}
+	double d = 0;
+	for (int row = 0; row < imgGrid.getSize()[0]; row++)
+		for (int col = 0; col < imgGrid.getSize()[1]; col++)
+			d = d + imgGrid.getAtIndex(row, col) * imgGrid.getAtIndex(row, col);
+	return d;
+}
 
 	
 }
