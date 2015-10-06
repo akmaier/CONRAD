5669b6f40377dd12065998df8c89ce1ada5677ff
YixingHuang <yixing.yh.huang@fau.de>
2015-10-06 13:14:00 +0200

    Update Perform3DwTV.java

--- src/edu/stanford/rsl/tutorial/weightedtv/Perform3DwTV.java
+++ src/edu/stanford/rsl/tutorial/weightedtv/Perform3DwTV.java
@@ -1,48 +1,30 @@
 package edu.stanford.rsl.tutorial.weightedtv;
+/**
+ * Here is an example to apply weighted total variation on 3D cone-beam limited angle tomography
+ */
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-
-import com.jogamp.opencl.CLCommandQueue;
 import com.jogamp.opencl.CLContext;
-
-import loci.poi.util.SystemOutLogger;
-import ij.IJ;
 import ij.ImageJ;
-import ij.ImagePlus;
 import edu.stanford.rsl.conrad.data.numeric.Grid2D;
 import edu.stanford.rsl.conrad.data.numeric.Grid3D;
-import edu.stanford.rsl.conrad.data.numeric.NumericPointwiseOperators;
 import edu.stanford.rsl.conrad.geometry.trajectories.Trajectory;
 import edu.stanford.rsl.conrad.opencl.OpenCLUtil;
 import edu.stanford.rsl.conrad.phantom.NumericalSheppLogan3D;
 import edu.stanford.rsl.conrad.utils.Configuration;
-import edu.stanford.rsl.conrad.utils.ImageUtil;
 import edu.stanford.rsl.tutorial.cone.ConeBeamProjector;
 import edu.stanford.rsl.tutorial.cone.ConeBeamBackprojector;
 import edu.stanford.rsl.conrad.data.numeric.opencl.OpenCLGrid2D;
 import edu.stanford.rsl.conrad.data.numeric.opencl.OpenCLGrid3D;
 
-public class Perform3DwTV {
+public class Perform3DWeightedTV {
 	static int maxIter=500;
 	static int maxTVIter=10;
-	static int showInt=10;
-	static int saveInt=5;
+	static int showInt=5;
 	static double eps=1.0e-10;
 	static double error=1,error2=1;
 	static int iter=0;
-	static boolean isSave=false;
-	static boolean debug=false;
 
-	
-	
-
-	static private String path="D:\\wTV3D\\";
-	static private File outPutDir=new File(path+"log.txt");
-	static private BufferedWriter bw;
-	
 	//geometry
 	static protected Trajectory geo = null;
 	static protected int maxProjs;
@@ -60,7 +42,7 @@ public class Perform3DwTV {
 	//
 	static protected CLContext context = null;
 	
-	//SART
+	//SART 
 	static float beta=0.8f;
 	protected static ConeBeamProjector cbp;
 	protected static ConeBeamBackprojector cbbp;
@@ -73,7 +55,7 @@ public class Perform3DwTV {
 	protected static OpenCLGrid3D normSino;
 	protected static OpenCLGrid3D normGrid;
 	
-	//WTV
+	//weighted TV
 	protected static TVGradient3D TVgrad;
 	protected static double step=0;
 	
@@ -81,17 +63,12 @@ public class Perform3DwTV {
 	public static void main(String[] args) throws Exception {
 		new ImageJ();
 			
-		
-		
-		if(!outPutDir.exists()){
-		    outPutDir.getParentFile().mkdirs();
-			outPutDir.createNewFile();
-		}
-		
-		bw=new BufferedWriter(new FileWriter(outPutDir));
-		
-		initCLDatastructure();
 
+		initOpenCLDataStructure();
+
+		/**
+		 * The ground truth phantom
+		 */
 		Grid3D phan = new NumericalSheppLogan3D(imgSizeX,imgSizeY, imgSizeZ).getNumericalSheppLoganPhantom();
 		phan.setSpacing(spacingX,spacingY,spacingZ);
 		volCL=new OpenCLGrid3D(phan);
@@ -100,46 +77,39 @@ public class Perform3DwTV {
 		cbp=new ConeBeamProjector();
 		cbbp=new ConeBeamBackprojector();
 		sinoP=new OpenCLGrid2D(new Grid2D(width,height));
-		 updBP = new OpenCLGrid3D(volCL);
+		updBP = new OpenCLGrid3D(volCL);
 		
 		getMeasuredSinoCL();		
 		initialReconCL();
 		createNormProj();		
 		createNormGrid();  
 	
-		
-		
-		
-		//***************wTV parameters*****************************
-			
-		
+		/**
+		 * weighted TV parameters
+		 */
 		TVgrad=new TVGradient3D(reconCL);
+		TVgrad.initialWeightMatrixCL();
 		
-		TVgrad.initialWmatrixCL();
 		
-		
-		//**********************************************************
-	
+		/**
+		 * output the TV value of the ground truth phantom
+		 */
 		long t_start=System.currentTimeMillis();
-		TVgrad.computeGradientCL(volCL);
+		TVgrad.computeImageGradientCL(volCL);
 		float pTV=volCL.getGridOperator().normL1(TVgrad.imgGradientCL);
 		System.out.println("Perfect TV: "+pTV);
 		
 		//**********************************************************
 		//Start iteration
 		while(iter<=maxIter&&error>eps){
-		   reconCLpre=new OpenCLGrid3D(reconCL);
-		
-					
-			sartiterate();//SART iteration
+		   reconCLpre=new OpenCLGrid3D(reconCL);				
+			sartIterate();//SART iteration
 
-			
 			//reconCL.show(" reconCL");
 			
 			//wTV part
-			wTViterate();
-			
-			TVgrad.wMatrixCLUpdate(reconCL);
+			weightedTVIterate();
+			TVgrad.weightMatrixCLUpdate(reconCL);
 
 			outPutError();	
 			iter++;
@@ -147,15 +117,16 @@ public class Perform3DwTV {
 		
 		long t_end=System.currentTimeMillis();
 		System.out.println("time is "+(t_end-t_start)/1000.0);
-		reconCL.show("final reconCL");
-		ImagePlus imp3=ImageUtil.wrapGrid3D(reconCL, null);
-		IJ.saveAs(imp3, "Tiff", (path+iter+"_"+"FinalReconCL.tif"));
-		bw.close();
-		
+		reconCL.show("final reconCL");		
 }
-	private static void sartiterate() throws Exception{
-	   OpenCLGrid2D tempSino;//=new OpenCLGrid2D(sinoP);
-	   //normGrid=new OpenCLGrid3D(volCL);//***********************
+	
+	/**
+	 * SART part
+	 * @throws Exception
+	 */
+	private static void sartIterate() throws Exception{
+	   OpenCLGrid2D tempSino;
+	   
 		for(int projIndex=0;projIndex<maxProjs;projIndex++){
 			
 			cbp.fastProjectRayDrivenCL(sinoP, reconCL, projIndex);
@@ -182,55 +153,47 @@ public class Perform3DwTV {
 	}
 	
 	
-	
-	private static void wTViterate() throws IOException{
+	/**
+	 * weighted TV gradient descent part
+	 * @throws IOException
+	 */
+	private static void weightedTVIterate() throws IOException{
 		OpenCLGrid3D tv_gradient;
-		double wTV=TVgrad.getwTVvalueCL(reconCL);
+		double wTV=TVgrad.getWeightedTVvalueCL(reconCL);
 		int i=0;
 		double deltaTV=-1.0;
 		while(i<maxTVIter){
 			double preTV=wTV;
-				tv_gradient=TVgrad.compute_wTV_Gradient(reconCL);
+				tv_gradient=TVgrad.computeWeightedTVGradient(reconCL);
 
 			tv_gradient.getGridOperator().divideBy(tv_gradient, tv_gradient.getGridOperator().max(tv_gradient));				
 	
-			wBTLsearch(tv_gradient);
+			backTrackingLineSearch(tv_gradient);
 			
-			wTV=TVgrad.getwTVvalueCL(reconCL);
+			wTV=TVgrad.getWeightedTVvalueCL(reconCL);
 			deltaTV=wTV-preTV;
 			//System.out.println("iter="+iter+" i="+i+" L1: "+reconCL.getGridOperator().normL1(TVgrad.imgGradientCL)+" wTV="+wTV+" step="+step);
 			System.out.println("iter="+iter+" i="+i+" wTV="+wTV+" step="+step);
-			if(debug)
-				//if(isSave)
-			{
-			ImagePlus imp3=ImageUtil.wrapGrid3D(reconCL, null);
-			IJ.saveAs(imp3, "Tiff", (path+iter+"_"+i+"thResult.tif"));
-			}
-			//bw.write("iter="+iter+" i="+i+" L1: "+reconCL.getGridOperator().normL1(TVgrad.imgGradientCL)+" wTV="+wTV+" step="+step+"\r\n");
-			bw.write("iter="+iter+" i="+i+" wTV="+wTV+" step="+step+"\r\n");
-			bw.flush();
 			i++;
 		}
 		
 	}
 
-	
-	private static void wBTLsearch( OpenCLGrid3D tv_grad){//weighted TV
+	/**
+	 * using back tracking line search to find the step size for weighted TV gradient descent
+	 * @param tv_grad
+	 */
+	private static void backTrackingLineSearch( OpenCLGrid3D tvGradient){//weighted TV
 		double t=1.0,tmin=0.0000001;
 		double alpha=0.3, beta=0.6;
 		double delta=1.0f,temp1,temp2;
 		
-		double TV=TVgrad.getwTVvalueCL(reconCL);
-		double Gradnorm=alpha*grid3DCLnorm(tv_grad);
-	
+		double TV=TVgrad.getWeightedTVvalueCL(reconCL);
+		double Gradnorm=alpha*openCLGrid3DNorm(tvGradient);
 		
-		tv_grad.getGridOperator().multiplyBy(tv_grad,(float) t);
-		
-		reconCL.getGridOperator().subtractBy(reconCL,tv_grad);
-
-		temp1=TVgrad.getwTVvalueCL(reconCL);
-		
-		
+		tvGradient.getGridOperator().multiplyBy(tvGradient,(float) t);
+		reconCL.getGridOperator().subtractBy(reconCL,tvGradient);
+		temp1=TVgrad.getWeightedTVvalueCL(reconCL);
 		temp2=t*Gradnorm;
 		delta=temp1-TV+temp2;
 		
@@ -238,13 +201,13 @@ public class Perform3DwTV {
 		{
 			
 			t=t*beta;
-			reconCL.getGridOperator().addBy(reconCL, tv_grad);
+			reconCL.getGridOperator().addBy(reconCL, tvGradient);
 			
-			tv_grad.getGridOperator().multiplyBy(tv_grad,(float) beta);
+			tvGradient.getGridOperator().multiplyBy(tvGradient,(float) beta);
 					
-			reconCL.getGridOperator().subtractBy(reconCL,tv_grad);
+			reconCL.getGridOperator().subtractBy(reconCL,tvGradient);
 
-			temp1=TVgrad.getwTVvalueCL(reconCL); 
+			temp1=TVgrad.getWeightedTVvalueCL(reconCL); 
 		
 			temp2=t*Gradnorm;
 			delta=temp1-TV+temp2;
@@ -253,8 +216,12 @@ public class Perform3DwTV {
 		step=t;
 	}
 	
-		
-	private static double grid3DCLnorm(OpenCLGrid3D reconCL) {
+	/**
+	 * L2 norm of OpenCLGrid3D	
+	 * @param reconCL
+	 * @return
+	 */
+	private static double openCLGrid3DNorm(OpenCLGrid3D reconCL) {
 		OpenCLGrid3D tempReconCL=new OpenCLGrid3D(reconCL);
 		tempReconCL.getGridOperator().multiplyBy(tempReconCL,tempReconCL);
 		double n=reconCL.getGridOperator().sum(tempReconCL);
@@ -262,16 +229,24 @@ public class Perform3DwTV {
 		return n;
 	}
 	
-	
-	private static double mse(OpenCLGrid3D gridCL1, OpenCLGrid3D gridCL2){
+	/**
+	 * mean square error
+	 * @param gridCL1
+	 * @param gridCL2
+	 * @return
+	 */
+	private static double meanSquareError(OpenCLGrid3D gridCL1, OpenCLGrid3D gridCL2){
 		OpenCLGrid3D tempCL=new OpenCLGrid3D(gridCL1);
 		tempCL.getGridOperator().subtractBy(tempCL, gridCL2);
-		double err=grid3DCLnorm(tempCL)/(gridCL1.getSize()[0]*gridCL1.getSize()[1]*gridCL1.getSize()[2]);
+		double err=openCLGrid3DNorm(tempCL)/(gridCL1.getSize()[0]*gridCL1.getSize()[1]*gridCL1.getSize()[2]);
 		tempCL.release();
 		return err;
 	}
 	
-	private static void initCLDatastructure(){
+	/**
+	 * initialize the geometry
+	 */
+	private static void initOpenCLDataStructure(){
 		Configuration.loadConfiguration();
 		Configuration conf = Configuration.getGlobalConfiguration();
 		geo = conf.getGeometry();
@@ -292,6 +267,10 @@ public class Perform3DwTV {
 
 	}
 	
+	/**
+	 * get the measured sinogram
+	 * @throws Exception
+	 */
 	protected static void getMeasuredSinoCL() throws Exception
 	{
 		sinoCL = new OpenCLGrid3D(new Grid3D(width,height,maxProjs));
@@ -300,16 +279,21 @@ public class Perform3DwTV {
 		sinoCL.show("sinoCL");
 	}
 	
+	/**
+	 * initialize the reconstructed image as 0
+	 */
 	protected static void initialReconCL()
 	{
-
 		reconCL=new OpenCLGrid3D(new Grid3D(imgSizeX,imgSizeY,imgSizeZ));
 		reconCL.setSpacing(spacingX,spacingY,spacingZ);
 		reconCL.getGridOperator().fill(reconCL, 0);
 	}
 		
 	
-	
+	/**
+	 * compute the normalization projections
+	 * @throws Exception
+	 */
 	protected static void createNormProj() throws Exception{
 		OpenCLGrid3D onesVol = new OpenCLGrid3D(new Grid3D(imgSizeX,imgSizeY,imgSizeZ));		
 		onesVol.getGridOperator().fill(onesVol, 1);
@@ -320,7 +304,12 @@ public class Perform3DwTV {
 	//if(debug)normSino.show("normSino");
 	}
 
-	protected static void createNormGrid2(int projIndex) throws Exception {
+	/**
+	 * compute the projIndex_th normalization grids for backprojection
+	 * @param projIndex
+	 * @throws Exception
+	 */
+	protected static void createNormGrid(int projIndex) throws Exception {
 		OpenCLGrid2D c_sinoCL=new OpenCLGrid2D(new Grid2D(width,height));		
 		c_sinoCL.getGridOperator().fill(c_sinoCL, 1.0f);
 		normGrid.release();
@@ -331,7 +320,10 @@ public class Perform3DwTV {
 		normGrid.getGridOperator().addBy(normGrid, (float)eps);
 	}
 	
-	
+	/**
+	 * compute the mean normalization grid for backprojection
+	 * @throws Exception
+	 */
 	protected static void createNormGrid() throws Exception {
 		OpenCLGrid2D c_sinoCL=new OpenCLGrid2D(new Grid2D(width,height));		
 		c_sinoCL.getGridOperator().fill(c_sinoCL, 1.0f);
@@ -341,34 +333,22 @@ public class Perform3DwTV {
 		normGrid.getGridOperator().fill(normGrid, (float)normGrid.getGridOperator().sum(normGrid)/(normGrid.getNumberOfElements()));			
 		//normGrid.getGridOperator().addBy(normGrid, (float)eps);
 	}
-	private static void outPutMediateResult( int iter, int i)
-	{
-			ImagePlus imprecon=ImageUtil.wrapGrid3D(reconCL, null);
-			IJ.saveAs(imprecon, "Tiff",(path+iter+"_"+i+"thImRecon.tif"));
-			TVgrad.computeGradientCL(reconCL);
-			ImagePlus imGrad=ImageUtil.wrapGrid3D(TVgrad.imgGradientCL, null);
-			IJ.saveAs(imGrad, "Tiff",(path+iter+"_"+i+"thImGrad.tif"));	
-			ImagePlus impTVgrad=ImageUtil.wrapGrid3D(TVgrad.tvGradientCL, null);
-			IJ.saveAs(impTVgrad, "Tiff", (path+iter+"_"+i+"thTVgradient.tif"));		
-	}
-	
+
+	/**
+	 * out put the result at each iteration
+	 * @throws IOException
+	 */
 	private static void outPutError() throws IOException{
 		
 		if (iter % showInt== 0)
 		{
 			reconCL.show(iter+"_th iteration");				
 		}
-		if(isSave)
-		if((iter<=100&&iter%saveInt==0)||(iter>100&&iter%50==0))
-		{
-			ImagePlus imp2=ImageUtil.wrapGrid3D(reconCL, null);
-			IJ.saveAs(imp2, "Tiff", (path+iter+"thResult.tif"));	
-		}
-		error =mse(reconCL, reconCLpre);	
-		error2=mse(reconCL, volCL);
+	
+		error =meanSquareError(reconCL, reconCLpre);	
+		error2=meanSquareError(reconCL, volCL);
 		System.out.println(iter+":  error=" + error+" error2= "+error2);	
-		bw.write(iter+":  error=" + error+" error2= "+error2+"\r\n");
-		bw.flush();
+	
 		reconCLpre.release();
 	}
 }
