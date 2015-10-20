ff704ca60ccc922fcdd5e2906ddd46292911cc15
Martin Berger <fi74tego@stud.informatik.uni-erlangen.de>
2015-10-20 11:31:48 +0200

    Test commit for Phantom file

---
+++ src/edu/stanford/rsl/tutorial/bergerma/Phantom.java
@@ -0,0 +1,12 @@
+package edu.stanford.rsl.tutorial.bergerma;
+
+import edu.stanford.rsl.conrad.data.numeric.Grid3D;
+
+public class Phantom extends Grid3D {
+
+	public Phantom(Grid3D input) {
+		super(input);
+		// TODO Auto-generated constructor stub
+	}
+
+}
