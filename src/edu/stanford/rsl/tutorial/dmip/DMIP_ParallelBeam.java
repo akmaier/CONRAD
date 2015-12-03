e8870b8bea5dfc3b27ee621c731da9c6d63534e5
PhilippSchlieper <Phil@fau-stud-10-207.rrze.uni-erlangen.de>
2015-12-03 17:03:20 +0100

    Parallel-Beam_Ex

--- src/edu/stanford/rsl/tutorial/dmip/DMIP_ParallelBeam.java
+++ src/edu/stanford/rsl/tutorial/dmip/DMIP_ParallelBeam.java
@@ -218,13 +218,39 @@ public class DMIP_ParallelBeam {
 		
 		if(filter == RampFilterType.RAMLAK)
 		{
-			// TODO: implement the ram-lak filter in the spatial domain 
+			final float odd = -1.0f / (float) Math.pow(Math.PI, 2);
 			
+			// TODO: implement the ram-lak filter in the spatial domain 
+			ramp.setAtIndex(0, 0.25f);
+			for (int i = 1; i < paddedSize/2; i++)
+			{
+				if (1 == (i % 2))
+				{
+					ramp.setAtIndex(i, odd / (float) Math.pow(i, 2)); 
+				}
+			}
+			for (int i = paddedSize/2; i < paddedSize; i++)
+			{
+				final float tmp = paddedSize - i;
+				if (1 == (i % 2))
+				{
+					ramp.setAtIndex(i, odd / (float) Math.pow(tmp, 2)); 
+				}
+			}
 		}
 		else if(filter == RampFilterType.SHEPPLOGAN)
 		{
 			// TODO: implement the Shepp-Logan filter in the spatial domain
-			
+			ramp.setAtIndex(0, 2.0f / (float) Math.pow(Math.PI, 2));
+			for (int i = 1; i < paddedSize/2; i++)
+			{
+				ramp.setAtIndex(i, -2.0f / (float) (Math.pow(Math.PI, 2) * (4 * Math.pow(i, 2) -1.0f))); 
+			}
+			for (int i = paddedSize/2; i < paddedSize; i++)
+			{
+				final float tmp = paddedSize - i;
+				ramp.setAtIndex(i, -2.0f / (float) (Math.pow(Math.PI, 2) * (4 * Math.pow(tmp, 2) -1.0f))); 
+			}
 		}
 		else
 		{
@@ -233,17 +259,21 @@ public class DMIP_ParallelBeam {
 		}
 		
 		// TODO: Transform ramp filter into frequency domain
+		ramp.transformForward();
 		
 		
 		Grid1DComplex sinogramF = new Grid1DComplex(sinogram,true);
 		// TODO: Transform the input sinogram signal into the frequency domain
-		
+		sinogramF.transformForward();
 		
 		// TODO: Multiply the ramp filter with the transformed sinogram
-		
+		for (int p = 0; p < sinogramF.getSize()[0]; p++)
+		{
+			sinogramF.multiplyAtIndex(p, ramp.getRealAtIndex(p), ramp.getImagAtIndex(p));
+		}
 		
 		// TODO: Backtransformation
-
+		sinogramF.transformInverse();
 		
 		// Crop the image to its initial size
 		Grid1D ret = new Grid1D(sinogram);
@@ -273,14 +303,14 @@ public class DMIP_ParallelBeam {
 		// size of a detector Element [mm]
 		float detectorSpacing = 1.0f;	
 		// filterType: NONE, RAMLAK, SHEPPLOGAN
-		RampFilterType filter = RampFilterType.NONE;				
+		RampFilterType filter = RampFilterType.SHEPPLOGAN;				
 		
 		// 1. Create the Shepp Logan Phantom
 		SheppLogan sheppLoganPhantom = new SheppLogan(phantomSize);
 		sheppLoganPhantom.show();
 		
 		// 2. Acquire forward projection images with a parallel projector
-		Grid2D sinogram = parallel.projectRayDriven(sheppLoganPhantom, angularRange, angularStepSize, detectorSize, detectorSpacing);
+		Grid2D sinogram = parallel.projectRayDriven(sheppLoganPhantom, angularRange-angularStepSize, angularStepSize, detectorSize, detectorSpacing);
 		sinogram.show("The Sinogram");
 		Grid2D filteredSinogram = new Grid2D(sinogram);
 		
