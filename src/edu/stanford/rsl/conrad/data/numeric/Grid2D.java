c23340d69f13bc4a4490cc02bf1768176be79c8c
PhysikerErlangen <PhysikerErlangen@users.noreply.github.com>
2016-07-22 17:34:28 +0200

    Update Grid2D.java

--- src/edu/stanford/rsl/conrad/data/numeric/Grid2D.java
+++ src/edu/stanford/rsl/conrad/data/numeric/Grid2D.java
@@ -97,6 +97,27 @@ public class Grid2D extends NumericGrid implements Transformable {
 		return subGrids[j];
 	}
 
+	@Deprecated
+	/**
+	 * Set the corresponding Grid1D object on the linear 2D row memory
+	 * 
+	 * CAUTION: We set absolute values and no references.
+	 * If the input Grid1D subGrid is changed at a later point,
+	 * it won't change the values in the Grid2D.
+	 * This is due to "float buffer" nature of Grid2D,
+	 * which is a fundamental difference to the structure of Grid3D,
+	 * which is defined by an ArrayList of Grid2Ds.
+	 * 
+	 * @param j The row-index (y-index, height-index)
+	 * @param subGrid
+	 */
+	public void setSubGrid(int j, Grid1D subGrid) {
+		
+        for (int i=0; i<subGrid.getSize()[0]; ++i) {
+            setAtIndex(j, i, subGrid.getAtIndex(i));
+        }
+        notifyAfterWrite();
+	}
 	
 	public double[] indexToPhysical(double i, double j) {
 		return new double[] {
