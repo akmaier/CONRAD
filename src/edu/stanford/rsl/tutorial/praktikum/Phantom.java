d4b024c88573b9de392f9f0e3f9a3a0dcf0a05f3
PhilippSchlieper <Phil@Philipps-MacBook-Pro.local>
2016-10-19 18:12:16 +0200

    Implemented 3rd geometric

--- src/edu/stanford/rsl/tutorial/praktikum/Phantom.java
+++ src/edu/stanford/rsl/tutorial/praktikum/Phantom.java
@@ -1,6 +1,8 @@
 package edu.stanford.rsl.tutorial.praktikum;
 
 import edu.stanford.rsl.conrad.data.numeric.Grid2D;
+import ij.ImageJ;
+
 
 public class Phantom extends Grid2D{
 
@@ -8,7 +10,7 @@ public class Phantom extends Grid2D{
 		super(width, height);
 		// TODO Auto-generated constructor stub
 		
-		//create square with intensity 100
+		//create square with intensity 0.2
 		int edgeLength = width/8;
 		for (int row =  edgeLength ; row < width/2 +  edgeLength ; row++) {
 			for (int col =  edgeLength ; col < height/2 +  edgeLength ; col++) {
@@ -16,7 +18,7 @@ public class Phantom extends Grid2D{
 			}
 		}
 		
-		//create circle with intensity 50
+		//create circle with intensity 0.5
 		int xCenter = width/2 + width/8;
 		int yCenter = height/2 + height/4;
 		int radius = width/10;
@@ -27,10 +29,24 @@ public class Phantom extends Grid2D{
 				}
 			}
 		}
+		
+		//create triangle with intensity 0.7
+		int count = 0;
+		for (int row = 2 * height/8; row < height/2; row++){
+			for (int col = 4 * width/8; col < width; col++){
+				if(col >= (6 * width/8) - count && col <= (6 * width/8) + count){
+					this.setAtIndex(col, row, (.7f));
+				}
+			}
+			count++;
+		}
 	}
 	
 	public static void main(String[] args){
+		ImageJ ui = new ImageJ();
 		Phantom a = new Phantom(256, 256);
+		
+		ui.createImage(256,256);
 		
 		a.show();
 	}
