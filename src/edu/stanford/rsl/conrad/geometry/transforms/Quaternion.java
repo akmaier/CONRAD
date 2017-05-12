e90cb3dafa7c2ed96d2018a4e3038a7b5da933d3
ichichich22 <krattrym@gmx.de>
2017-05-12 14:37:00 +0200

    Added functionality to quaternion class

--- src/edu/stanford/rsl/conrad/geometry/transforms/Quaternion.java
+++ src/edu/stanford/rsl/conrad/geometry/transforms/Quaternion.java
@@ -99,6 +99,61 @@ public class Quaternion {
 		Quaternion tmp = this.multipliedBy( q );
 		return 2*Math.toDegrees( Math.acos( tmp.getScaler() ) );
 	}
+	
+	public double length(){
+		return Math.sqrt(scaler*scaler + vector.getElement(0)*vector.getElement(0) + vector.getElement(1)*vector.getElement(1) + vector.getElement(2)*vector.getElement(2));
+	}
+	
+	/**
+	 * Calculates a quaternion with a given rotation matrix.
+	 * The direct way of creating linear equations from the relations given in function "equivalentMatrix()" is computationally intensive and can lead to "Gimbal lock".
+	 * The solution can be determined in two steps:
+	 * First, the maximum element of the quaternion has to be determined. The equation 
+	 * 		R11 + R22 + R33 = (a*a + b*b - c*c - d*d) + (a*a - b*b + c*c - d*d) + (a*a - b*b - c*c + d*d)
+	 * can be solved for every element of the quaternion, where "R11 + R22 + R33" is the trace of the rotation matrix.
+	 * The maximum element is needed to avoid dividing by zero in the following steps. The result is not influenced by this choice. 
+	 * Equations taken from: Prof. Dr.-Ing. Jörg Buchholz, 2010, Regelungstechnik und Flugregler, Munich, GRIN Verlag, http://www.grin.com/de/e-book/82818/regelungstechnik-und-flugregler
+	 * @param R
+	 * @return q
+	 */
+	public Quaternion equivalentQuaternion(SimpleMatrix R){
+		Quaternion q;
+		//find maximum element
+		double scalarNew = Math.sqrt(R.getElement(1, 1) + R.getElement(2, 2) + R.getElement(3, 3) + 1) / 2;
+		double vectorX = Math.sqrt(R.getElement(1, 1) - R.getElement(2, 2) - R.getElement(3, 3) + 1) / 2;
+		double vectorY = Math.sqrt(-R.getElement(1, 1) + R.getElement(2, 2) - R.getElement(3, 3) + 1) / 2;
+		double vectorZ = Math.sqrt(-R.getElement(1, 1) - R.getElement(2, 2) + R.getElement(3, 3) + 1) / 2;
+		double maxEl = Math.max(Math.max(scalarNew, vectorX), Math.max(vectorY, vectorZ));
+		//determine other elements with the found maximum
+		if (maxEl == scalarNew){
+			vectorX 	= (R.getElement(2, 3) - R.getElement(3, 2)) / (4 * maxEl);
+			vectorY 	= (R.getElement(3, 1) - R.getElement(1, 3)) / (4 * maxEl);
+			vectorZ 	= (R.getElement(1, 2) - R.getElement(2, 1)) / (4 * maxEl);
+			SimpleVector vectorNew = new SimpleVector(vectorX, vectorY, vectorZ);
+			q = new Quaternion(scalarNew, vectorNew);
+		}else if (maxEl == vectorX){
+			scalarNew 	= (R.getElement(2, 3) - R.getElement(3, 2)) / (4 * maxEl);
+			vectorY 	= (R.getElement(1, 2) + R.getElement(2, 1)) / (4 * maxEl);
+			vectorZ 	= (R.getElement(1, 3) + R.getElement(3, 1)) / (4 * maxEl);
+			SimpleVector vectorNew = new SimpleVector(maxEl, vectorY, vectorZ);
+			q = new Quaternion(scalarNew, vectorNew);
+		}else if (maxEl == vectorY){
+			scalarNew 	= (R.getElement(3, 1) - R.getElement(1, 3)) / (4 * maxEl);
+			vectorX 	= (R.getElement(1, 2) + R.getElement(2, 1)) / (4 * maxEl);
+			vectorZ 	= (R.getElement(2, 3) + R.getElement(3, 2)) / (4 * maxEl);
+			SimpleVector vectorNew = new SimpleVector(vectorX, maxEl, vectorZ);
+			q = new Quaternion(scalarNew, vectorNew);
+		}else if (maxEl == vectorZ){
+			scalarNew 	= (R.getElement(1, 2) - R.getElement(2, 1)) / (4 * maxEl);
+			vectorX 	= (R.getElement(1, 3) + R.getElement(3, 1)) / (4 * maxEl);
+			vectorY 	= (R.getElement(2, 3) + R.getElement(3, 2)) / (4 * maxEl);
+			SimpleVector vectorNew = new SimpleVector(vectorX, vectorY, maxEl);
+			q = new Quaternion(scalarNew, vectorNew);
+		}else{
+			q = new Quaternion(0, new SimpleVector());
+		}
+		return q;
+	}
 }
 /*
  * Copyright (C) 2010-2014 Andreas Maier, Rotimi X Ojo
