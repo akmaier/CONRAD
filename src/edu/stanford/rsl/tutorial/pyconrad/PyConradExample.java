79b7eb6dfec28ba0ecf99ac53a1269f9007e4757
tmgeimer <tobias.geimer@fau.de>
2018-11-22 11:48:41 +0100

    Added copyright notice to PyConradExample.java

--- src/edu/stanford/rsl/tutorial/pyconrad/PyConradExample.java
+++ src/edu/stanford/rsl/tutorial/pyconrad/PyConradExample.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2010-2018 Stefan Seitz
+ * CONRAD is developed as an Open Source project under the GNU General Public License (GPL).
+*/
 package edu.stanford.rsl.tutorial.pyconrad;
 
 import edu.stanford.rsl.conrad.pyconrad.PyConrad;
